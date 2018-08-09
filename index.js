07a92bb2f82013c804b7fb239fa1d9b6a737020a
Salvatore Previti <roorback@gmail.com>
2018-08-09 17:37:21 +0100

    initial

---
+++ index.js
@@ -0,0 +1,7 @@
+const defaultOptions = require('./lib/defaultOptions')
+const readSingleAsync = require('./lib/readSingleAsync')
+const readDirAsync = require('./lib/readDirAsync')
+
+module.exports.defaultOptions = defaultOptions
+module.exports.readSingleAsync = readSingleAsync
+module.exports.readDirAsync = readDirAsync
